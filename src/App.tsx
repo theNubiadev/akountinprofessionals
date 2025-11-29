64c50b5865e4e4b7461fae0b03c07997f8442117
theNubiadev <ajaiyeobajibola@gmail.com>
2025-11-29 11:29:27 +0100

    installed analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { Analytics } from "@vercel/analytics/next"
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
+    <Analytics />
     <TooltipProvider>
       <Toaster />
       <Sonner />
