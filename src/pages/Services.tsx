1b5aca20c93b8fa4eb5819d618be910ae8bda5cb
theNubiadev <ajaiyeobajibola@gmail.com>
2025-12-01 07:43:25 +0100

    corrected the workflow

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,14 +1,14 @@
 import Footer from "@/components/Footer"
 import Navbar from "@/components/Navbar"
 // import Services from "@/components/Services"
-import Services from "@/components/Services.tsx";
+// import Services from "@/components/Services.tsx";
 export default function Services() {
     return (
         <>
             <div className="min-h-screen ">
                 <Navbar />
                 {/* <Services /> */}
-                <Services />
+                {/* <Services /> */}
                 <Footer />
             </div>
         </>
